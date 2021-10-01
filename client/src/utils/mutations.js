677f480b93bc4a9efc2e77100b608dee16797e0e
Lance Bailey <lancebailey222@gmail.com>
2021-10-01 17:40:55 -0400

    adding mutation+queries for update

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+
+`;
+export const ADD_USER = gql`
+
+`;
+export const SAVE_BOOK = gql`
+
+`;
+
+export const REMOVE_BOOK = gql`
+
+`;
